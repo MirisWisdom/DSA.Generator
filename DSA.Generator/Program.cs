f34f0814ac84d89bc2960af1571b7a0b4b082fd3
Emilian Roman <roman.emilian@outlook.com>
2018-11-12 07:57:12 +0800

    Initialise repository with the original prototype

---
+++ DSA.Generator/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace DSA.Generator
+{
+    /// <summary>
+    /// Generates a DSA sequence and writes it to the console.
+    /// </summary>
+    internal static class Program
+    {
+        /// <summary>
+        /// Length of the DSA generated string.
+        /// </summary>
+        private const int Length = 64;
+
+        /// <summary>
+        /// DSA-compliant characters.
+        /// </summary>
+        private static readonly string[] Characters = {
+            "D", "S", "A", "<", ">", "?"
+        };
+        
+        /// <summary>
+        /// Main loop.
+        /// </summary>
+        public static void Main()
+        {
+            var random = new Random();
+            
+            // accelerate heat death of the universe
+            while (true)
+            {
+                var result = new string[Length];
+
+                // populate each index in the empty string array with a randomly chosen DSA character
+                for (int i = 0; i < result.Length; i++)
+                {
+                    result[i] = Characters[random.Next(0, Characters.Length - 1)];
+                }
+                
+                // write the array to the console by joining the items
+                Console.WriteLine(string.Join(string.Empty, result));
+            }
+        }
+    }
+}
