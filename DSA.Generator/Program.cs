1b465de806854b6e3cc7abc139f02e4fa44727f5
Emilian Roman <roman.emilian@outlook.com>
2018-11-12 18:51:12 +0800

    Clean up code & improve consistency

--- DSA.Generator/Program.cs
+++ DSA.Generator/Program.cs
@@ -3,28 +3,30 @@
 namespace DSA.Generator
 {
     /// <summary>
-    /// Generates a DSA sequence and writes it to the console.
+    ///     Generates a DSA sequence and writes it to the console.
     /// </summary>
     internal static class Program
     {
         /// <summary>
-        /// DSA-compliant characters.
+        ///     DSA-compliant characters.
         /// </summary>
-        private static readonly string[] DsaCharacters = {
+        private static readonly string[] DsaCharacters =
+        {
             "A", "S", "D", "<", ">", "?"
         };
 
         /// <summary>
-        /// LO7-compliant characters.
+        ///     LO7-compliant characters.
         /// </summary>
-        private static readonly string[] Lo7Characters = {
+        private static readonly string[] Lo7Characters =
+        {
             "L", "O", "A", "7"
         };
-        
+
         /// <summary>
-        /// Main loop.
+        ///     Main loop.
         /// </summary>
-        public static void Main(string [] args)
+        public static void Main(string[] args)
         {
             if (args.Length == 0)
             {
@@ -45,25 +47,22 @@ namespace DSA.Generator
                 Console.WriteLine("Invalid argument provided. Please input LO7 or DSA.");
                 Environment.Exit(2);
             }
-            
+
             var random = new Random();
-            
+
             // accelerate heat death of the universe
             while (true)
             {
-                var result = new string[random.Next(48,64)];
+                var result = new string[random.Next(48, 64)];
 
-                // initiate string with 'ASD'/'LOA' string bias.
+                // initiate string with 'ASD'/'LOA' string bias
                 result[0] = characters[0];
                 result[1] = characters[1];
                 result[2] = characters[2];
 
                 // populate each index in the empty string array with a randomly chosen DSA character
-                for (int i = 3; i < result.Length; i++)
-                {
-                    result[i] = characters[random.Next(0, characters.Length)];
-                }
-                
+                for (int i = 3; i < result.Length; i++) result[i] = characters[random.Next(0, characters.Length)];
+
                 // write the array to the console by joining the items
                 Console.WriteLine(string.Join(string.Empty, result));
             }
